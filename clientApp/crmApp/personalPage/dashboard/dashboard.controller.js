00d1dc5c0a796b82e1fe7ed5188e8e0ef7bc6375
Mikhail Mikhalev <Mikhail Mikhalev>
2016-05-23 21:45:02 +0300

    Сделан календарь на странице Рабочий стол

--- clientApp/crmApp/personalPage/dashboard/dashboard.controller.js
+++ clientApp/crmApp/personalPage/dashboard/dashboard.controller.js
@@ -2,5 +2,73 @@ angular.module('crmApp').controller("dashboardCtrl", ["$stateParams",
      function ($stateParams) {
          var vm = this;
          //alert("dashboard: " + $stateParams.company_id);
+         vm.calendar = testCalendar;
      }
 ]); 
+
+
+var testCalendar = [
+             {
+                 value: 1,
+                 name:  "Среда",
+                 month: "Июнь",
+                 today: false,
+                 tasks:
+                 [
+                     {
+                         name: "Позвонить клиенту",
+                         time: "15:00",
+                         type: "call",
+                         id: 111
+                     },
+                     {
+                         name: "Встреча для обсуждения деталей ТЗ",
+                         time: "18:00",
+                         type: "meeting",
+                         id: 112
+                     }
+                 ]
+             },
+             {
+                 value: 2,
+                 name:  "Четверг",
+                 month: "Июнь",
+                 today: true,
+                 tasks:
+                 [
+                     {
+                         name: "Позвонить клиенту",
+                         time: "15:00",
+                         type: "call",
+                         id: 211
+                     },
+                     {
+                         name: "Встреча для обсуждения деталей ТЗ",
+                         time: "18:00",
+                         type: "meeting",
+                         id: 212
+                     }
+                 ]
+             },
+             {
+                 value: 3,
+                 name:  "Пятница",
+                 month: "Июнь",
+                 today: false,
+                 tasks:
+                 [
+                     {
+                         name: "Позвонить клиенту",
+                         time: "15:00",
+                         type: "call",
+                         id: 311
+                     },
+                     {
+                         name: "Встреча для обсуждения деталей ТЗ",
+                         time: "18:00",
+                         type: "meeting",
+                         id: 312
+                     }
+                 ]
+             },
+         ];
