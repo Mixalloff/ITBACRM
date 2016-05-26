3603e339be92c9276978e05d74baef4a468d68d1
Mixalloff <mikhail.mikhalev37@gmail.com>
2016-05-26 17:46:47 +0300

    Добавлена страница настроек Воронка

--- clientApp/crmApp/crmApp.module.js
+++ clientApp/crmApp/crmApp.module.js
@@ -1,4 +1,4 @@
-angular.module('crmApp', ['ngMaterial', 'ui.router', 'ngResource', 'ngCookies'])
+angular.module('crmApp', ['ngMaterial', 'ui.router', 'ngResource', 'ngCookies', 'mdColorPicker'])
 .run(['$http', '$cookies', function($http, $cookies) {
         $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
         $http.defaults.headers.common['X-CSRFToken'] = $cookies.csrftoken;
--- clientApp/crmApp/personalPage/settings/sales_funnel/settings.sales_funnel.controller.js
+++ clientApp/crmApp/personalPage/settings/sales_funnel/settings.sales_funnel.controller.js
@@ -1,5 +1,52 @@
 angular.module('crmApp').controller("settings_sales_funnelCtrl", ["$stateParams",
      function ($stateParams) {
          var vm = this;
+         vm.config = config;
+         vm.setColor = setColor;
      }
 ]); 
+
+function setColor(color){
+    return { "background-color": color };
+}
+
+var config = {
+    sales_funnel: {
+        stages: [
+            {
+                id: 1,
+                num: 1, // порядковый номер этапа
+                name: "Первичный контакт",
+                color: "#00F",
+            },
+            {
+                id: 2,
+                num: 2, // порядковый номер этапа
+                name: "Обсуждение деталей",
+                color: "#F0F",
+            },
+            {
+                id: 0,
+                num: 1,
+                name: "Заявки",
+                color: "#FF0",
+                system: "LEADS"
+            },
+            {
+                id: 1000,
+                num: 1000,
+                name: "Завершенные сделки",
+                color: "#0F0",
+                system: "DONE"
+            },
+            {
+                id: -1,
+                num: -1,
+                name: "Потерянные сделки",
+                color: "#F00",
+                system: "CANCELED"
+            }
+        ],
+        
+    }
+}
